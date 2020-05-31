f5167267629f6f86b58184d9d5127da73a080ca5
bellycoder <rjazminaguilar@gmail.com>
2020-05-30 21:46:26 -0500

    Saas integration

---
+++ src/Componentes/Canvas.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Canvas.css'
+
+export default class Canvas extends Component {
+    render() {
+        return (
+            <div className="canva">
+                <h1>Juego de la vida</h1>
+            </div>
+        )
+    }
+}
