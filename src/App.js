8ef4c8f3bf242c83b61349580b9d08d5f5384218
bellycoder <rjazminaguilar@gmail.com>
2020-05-30 21:55:29 -0500

    Canva.js component linked to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Canvas from './Componentes/Canvas'
 
 function App() {
   return (
     <div className="App">
-     
+      <Canvas/>
     </div>
   );
 }
