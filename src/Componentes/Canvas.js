2cc49aa9073912fbb0fef1f456ef314b5d1d6851
bellycoder <rjazminaguilar@gmail.com>
2020-05-30 22:42:33 -0500

    Set background condovive color

--- src/Componentes/Canvas.js
+++ src/Componentes/Canvas.js
@@ -5,7 +5,7 @@ export default class Canvas extends Component {
     render() {
         return (
             <div className="canva">
-                <h1>Juego de la vida</h1>
+        
             </div>
         )
     }
